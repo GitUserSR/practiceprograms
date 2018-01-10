5db2841ef2e516a35855d6e6e86a919919c71fe8
GitUserSR <rapolu.swathi@gmail.com>
2018-01-10 13:17:30 -0800

    20180110 commit

--- src/main/java/database/mongodb/MongoClientTest.java
+++ src/main/java/database/mongodb/MongoClientTest.java
@@ -1,7 +1,9 @@
 package database.mongodb;
 
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.client.ListCollectionsIterable;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 import org.bson.Document;
@@ -12,21 +14,22 @@ import org.bson.Document;
 public class MongoClientTest {
     public static void main(String[] args) {
 
-        MongoClient mongoClient = new MongoClient();
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
 
         MongoIterable<String> mongoDatabaseNames = mongoClient.listDatabaseNames();
         for (String mongoDatabaseName : mongoDatabaseNames) {
             System.out.println("MongoClientTest: " + mongoDatabaseName);
         }
 
-        MongoDatabase mbureauDatabase = mongoClient.getDatabase("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz1111111111");
-        ListCollectionsIterable<Document> mbureauTables = mbureauDatabase.listCollections();
-        for (Document mbureauTable: mbureauTables ) {
-            System.out.println("mbureauTable: " + mbureauTable);
+        MongoDatabase database = mongoClient.getDatabase("local");
+        ListCollectionsIterable<Document> tables = database.listCollections();
+        for (Document table: tables ) {
+            System.out.println("mbureauTable: " + table);
         }
 
-
-
-
+        
+        MongoCollection<Document> weigntSensorTable =  database.getCollection("weightsensor");
+        
+        
     }
 }
